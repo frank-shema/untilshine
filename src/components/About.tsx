77590a570269bb7206eb21595d259d1badb43101
frank-shema <shemafranko0987@gmail.com>
2023-07-26 01:29:08 +0200

    added a gap

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,17 +15,17 @@ export default function About() {
 					<img src={ring} alt="" className='w-[85%]' />
 					<img src={people} alt="" className='absolute top-[30%] h-[50%] -right-5 rounded-md  border' />
 				</div>
-				<div className="w-full md:w-[50%] text-white text-[20px] pl-5">
-					<div >
+				<div className="w-full md:w-[50%] text-white text-[20px] pl-5 flex flex-col gap-4">
+					<div className='flex flex-col gap-1'>
 						<h1 className="text-[#FFDB00] font-medium text-sm lg:text-base">Us</h1>
 						<p className='text-[10px] lg:text-sm'>we are passionate about gaming and dedicated to creating extraordinary experiences for players all around the world. Founded in [Year of Establishment], our company has emerged as a leading force in the gaming industry, driven by innovation, creativity, and an unwavering commitment to delivering top-notch gaming content.</p>
 					</div>
-					<div>
+					<div className='flex flex-col gap-1'>
 						<h1 className="text-[#FFDB00] font-medium text-sm lg:text-base">Our mission</h1>
 						<p className='text-[10px] lg:text-sm'>Our mission is simple yet powerful - to craft captivating and immersive gaming experiences that resonate with players of all ages and backgrounds. We strive to create games that not only entertain but also challenge the boundaries of imagination, pushing the limits of interactive storytelling, gameplay mechanics, and visual aesthetics.</p>
 					</div>
 
-					<div>
+					<div className='flex flex-col gap-1'>
 						<h1 className="text-[#FFDB00] font-medium text-sm lg:text-base">Join Us on this Journey</h1>
 						<p className='text-[10px] lg:text-sm'>Join us on an exhilarating journey through the realms of imagination and gaming excellence. At our Company, we look forward to welcoming you into our ever-growing family of gamers, where you can forge friendships, conquer challenges, and create memories that last a lifetime.</p>
 					</div>
